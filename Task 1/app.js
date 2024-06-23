b958decda12b48afc5ed3885d42d50bbedfdb846
Nikhil422004 <133030301+Nikhil422004@users.noreply.github.com>
2024-06-23 11:54:46 +0530

    Complete commit of Task 1

---
+++ Task 1/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.listen(3000);
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let products = [
+  {
+    name: "Football",
+    brand: "Nivia Shining Star, Size 5",
+    price: "900",
+    date: "24-06-2024",
+    desc: "Premium stitched foortball for all types of terrain. Comes with a 2 month replacement policy. BUY NOW!!",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.render("index", { products });
+});
+
+app.get("/home", (req, res) => {
+  res.redirect("/");
+});
+
+app.post("/add-product", (req, res) => {
+  let newProduct = req.body;
+  products.push(newProduct);
+  res.redirect("/");
+});
+
+app.use((req, res) => {
+  res.status(404).render("error");
+});
